39c9a0a39218edc4b0d5493a70e4e4205a8282e8
chloerihouay <chloerihouay@hotmail.com>
2020-08-25 16:58:22 +0200

    search

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,9 +1,11 @@
 class RidesController < ApplicationController
   def search
     if params[:query].present?
-      @ride = Ride.create(destination_address: params[:query])
+      results = Geocoder.search(params[:query])
+      # @ride = Ride.create(destination_address: params[:query])
+      @markers = [{ lat: results.first.coordinates.first, lng: results.first.coordinates.last }]
     end
 
-    @markers = [{ lat: @ride.destination_latitude, lng: @ride.destination_longitude }]
+    # @markers = [{ lat: @ride.destination_latitude, lng: @ride.destination_longitude }]
   end
 end
