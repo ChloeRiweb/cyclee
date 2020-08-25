4e24963a75cf7c2869cfd7c647593c644faea695
chloerihouay <chloerihouay@hotmail.com>
2020-08-25 12:14:55 +0200

    map displayed on /search

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,7 @@ class Ride < ApplicationRecord
   belongs_to :user
   has_many :dangers
   has_one :parking
+
+  geocoded_by :destination_address, latitude: :destination_latitude, longitude: :destination_longitude
+  after_validation :geocode, if: :will_save_change_to_destination_address?
 end
