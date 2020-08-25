3253b313ed44d3272755d88e62010a9677115d45
Mathilde Nauche <m.nauche@gmail.com>
2020-08-25 17:14:49 +0200

    create ride done

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,4 @@
 class RidesController < ApplicationController
-
   def search
     # @markers = @rides.map do |ride|
     #   {
@@ -7,6 +6,22 @@ class RidesController < ApplicationController
     #     lng: ride.destination_longitude
     #   }
     # end
+    @ride = Ride.new
   end
 
+  def create
+    @ride = Ride.new(ride_params)
+    @ride.user = current_user
+    if @ride.save
+      redirect_to ride_path(@ride)
+    else
+      render :search
+    end
+  end
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:origin_latitude, :origin_longitude, :destination_address)
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,6 +3,8 @@ class Ride < ApplicationRecord
   has_many :dangers
   has_one :parking
 
+  validates :origin_latitude, :origin_longitude, :destination_address, presence: true
+
   geocoded_by :destination_address, latitude: :destination_latitude, longitude: :destination_longitude
   after_validation :geocode, if: :will_save_change_to_destination_address?
 end
