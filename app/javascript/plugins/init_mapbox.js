fc579d00298e06478c1812e50c0f0949c8802a7c
chloerihouay <chloerihouay@hotmail.com>
2020-08-26 16:33:31 +0200

    refacto

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,7 +9,6 @@ const fitMapToMarkers = (map, markers) => {
 
 
 const addMarker = async (map) => {
-  // console.log(map)
   const position = await getCurrentPosition();
   fillRideForm(position.coords.longitude, position.coords.latitude)
 
@@ -63,50 +62,6 @@ const initMapbox = () => {
   }
 };
 
-// je récupère la current position du navigateur :
-// 1/ fonction de rappel qui prend un objet Position comme argument.
-// const success = (position) => {
-//   console.log(position)
-  // const coords = position.coords;
-  //   console.log(coords)
-  //   console.log('Votre position actuelle est :');
-  //   console.log(`Latitude : ${coords.latitude}`);
-  //   console.log(`Longitude : ${coords.longitude}`);
-  // const lat = document.getElementById('ride_origin_latitude');
-  // const long = document.getElementById('ride_origin_longitude');
-  //   console.log(lat);
-  //   console.log(long);
-  // lat.value = coords.latitude;
-  // long.value = coords.longitude;
-  // const markers = [{coords.longitude, coords.latitude}];
-  // console.log(markers);
-  // if (markers) {
-  //   markers.forEach((marker) => {
-  //     console.log(marker)
-  //     new mapboxgl.Marker()
-  //       .setLngLat([ marker.lng, marker.lat ])
-  //       .addTo(map);
-  //   });
-  //   fitMapToMarkers(map, markers);
-  //   map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-  //                                     mapboxgl: mapboxgl }));
-  // };
-  // const marker = new mapboxgl.Marker()
-  //   .setLngLat([coords.longitude, coords.latitude])
-  //   .addTo(map);
-  //   console.log(marker);
-// }
-
-
-
-// 2/ j'appelle la méthode getCurrentPosition
-// const getCurrentPosition = () => {
-//   navigator.geolocation.getCurrentPosition((position) => {
-//     return position
-//   });
-// };
-
-
 const fillRideForm = (long, lat) => {
   const latInput = document.getElementById('ride_origin_latitude');
   const longInput = document.getElementById('ride_origin_longitude');
@@ -121,11 +76,5 @@ const getCurrentPosition = () => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
-
-// 3/ j'insère les coordonnées dans les champs origin_lat & origin_long
-
-// 4/ ajouter le marqueur
-
-//
 
 export { initMapbox, getCurrentPosition };
