3a02f9445cf46d7ef36d2c20d19f2aa7f32dabed
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-08-28 17:57:35 +0200

    creation d'un nouveau danger et modif de la show avec le lien vers new_danger

--- app/controllers/dangers_controller.rb
+++ app/controllers/dangers_controller.rb
@@ -1,2 +1,20 @@
 class DangersController < ApplicationController
+  def new
+    @danger = Danger.new
+  end
+
+  def create
+    @danger = Danger.new(danger_params)
+    if @danger.save!
+      redirect_to ride_path(@ride)
+    else
+      render "danger/new"
+    end
+  end
+
+  private
+
+  def danger_params
+    params.require(:danger).permit(:category, :latitude, :longitude)
+  end
 end
