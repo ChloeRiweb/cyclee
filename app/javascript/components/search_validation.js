eeac538946acfe7515a00a27937a2806fb4eba55
Mathilde Nauche <m.nauche@gmail.com>
2020-08-28 13:01:22 +0200

    Front 1ere page search done SAUF map bleu

---
+++ app/javascript/components/search_validation.js
@@ -0,0 +1,15 @@
+const searchValidation = () => {
+  const searchIcon = "<i class='fas fa-search'></i>";
+  const searchForm = document.querySelector("#rides-search .search-form");
+  const searchBtn = document.querySelector("#rides-search .search-form .btn-secondary");
+
+  searchForm.insertAdjacentHTML("beforeend", searchIcon);
+
+  const searchIconValidation = document.querySelector(".fa-search");
+
+  searchIconValidation.addEventListener("click", (event) => {
+    searchBtn.click();
+  })
+}
+
+export { searchValidation };
