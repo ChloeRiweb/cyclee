7c731d323dc6d9bab5164b39fed31103c63cd3da
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-08-30 16:24:26 +0200

    la modal est prete, maintenant creation des liens

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "search", to: "rides#search"
 
-  resources :rides, only: [:create, :show, :edit, :update] do
+  resources :rides, only: [:index, :create, :show, :edit, :update] do
     resources :dangers, only: [:new, :create]
   end
 end
