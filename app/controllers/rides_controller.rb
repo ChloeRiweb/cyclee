d4c358e7cdea7b072af097aa1fce80ff94ceda28
zsh git_setup.sh <leonie.elody@gmail.com>
2020-08-31 18:42:47 +0200

    Fix 2 path

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -32,7 +32,7 @@ class RidesController < ApplicationController
 
   def edit
     @cycling_waypoints = get_waypoints(@ride, 'cycling')[0]['routes'][0]['geometry']['coordinates']
-    @cycling_waypoints_alt = get_waypoints_alt(@ride, 'cycling')[0]['routes'][0]['geometry']['coordinates']
+    @cycling_waypoints_alt = get_waypoints_alt(@ride, 'driving')[0]['routes'][0]['geometry']['coordinates']
   end
 
   def update
