b529cc9d33594edec42d107a72155e83796ddc55
chloerihouay <chloerihouay@hotmail.com>
2020-09-02 11:16:05 +0200

    fix zoom trajet

--- app/javascript/plugins/init_mapbox_edit.js
+++ app/javascript/plugins/init_mapbox_edit.js
@@ -5,7 +5,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 20, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 20, maxZoom: 12, duration: 0 });
 };
 
 const initMapboxEdit = () => {
